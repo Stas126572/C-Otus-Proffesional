c34074d092c28f12165e28f4050d51698b13b6d2
Stas126572 <manichev_ss@mail.ru>
2025-06-23 09:04:50 +0300

    Update mapper.cpp

--- mapper.cpp
+++ mapper.cpp
@@ -2,25 +2,30 @@
 #include <string>
 
 
+int mapper(std::string line)
+{
+    std::string the_needly_data;
+    for (auto i : line)
+    {
+        if (i == ',')
+        {
+            the_needly_data = "";
+        }
+        else
+        {
+            the_needly_data += i;
+        }
+    }
+    int r = std::stoi(the_needly_data);
+    return r;
+}
+
 int main(int argc, char ** argv)
 {
     std::string line;
     while (std::getline(std::cin, line))
     {
-		std::string the_needly_data;
-		for (auto i : line)
-		{
-			if (i == ',')
-			{
-				the_needly_data = "";
-			}
-			else
-			{
-				the_needly_data += i;
-			}
-		}
-    		int r = std::stoi(the_needly_data);
-		std::cout << r << std::endl;
+        std::cout << mapper(line) << std::endl;
     }
 
     return 0;
