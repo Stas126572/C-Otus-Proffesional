4167591c0c50995cba3611785ac15d6ab2a20bc0
Stas126572 <manichev_ss@mail.ru>
2025-06-23 09:05:57 +0300

    Update mapper_quadretic.cpp

--- mapper_quadretic.cpp
+++ mapper_quadretic.cpp
@@ -1,42 +1,53 @@
 #include <iostream>
 #include <string>
 
-double mapper(int value, double sredn_)
+std::pair<double, bool> mapper(double needly_data, std::string line)
 {
-	double value_ = value;
-	value_ -= sredn_;
-	value_ *= value_;
-	return value_;
+     std::string the_needly_data;
+     for (auto i : line)
+     {
+         if (i == ',')
+         {
+             the_needly_data = "";
+         }
+         else
+         {
+             the_needly_data += i;
+         }
+     }
+
+     try
+     {
+        int value = std::stoi(the_needly_data);
+        double value_ = value;
+        value_ -= needly_data;
+        value_ *= value_;
+        return std::make_pair(value_, true);
+     }
+     catch(...)
+     {
+        return std::make_pair(0, false);
+         //std::cout << the_needly_data << " " << line << std::endl;
+     }
+
+
 }
 
 int main(int argc, char ** argv)
 {
     std::string line;
-    double sredn_ = std::atof(argv[1]);
+    double needly_data = std::atof(argv[1]);
     while (std::getline(std::cin, line))
     {
-		std::string the_needly_data;
-		for (auto i : line)
-		{
-			if (i == ',')
-			{
-				the_needly_data = "";
-			}
-			else
-			{
-				the_needly_data += i;
-			}
-		}
-		
-    		try
-		{
-			int r = std::stoi(the_needly_data);
-			std::cout << mapper(r, sredn_) << std::endl;
-		}
-		catch(...)
-		{
-			//std::cout << the_needly_data << " " << line << std::endl;
-		}
+        bool IsCorrect;
+        auto res = mapper(needly_data, line);
+        std::tie(std::ignore, IsCorrect) = res;
+        if (IsCorrect)
+        {
+            double print_res;
+            std::tie(print_res, std::ignore) = res;
+            std::cout << print_res;
+        }
     }
 
     return 0;
