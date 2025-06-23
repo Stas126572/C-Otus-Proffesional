8c98b51e12b79403e13741992961b7a8c8bf030d
Stas126572 <manichev_ss@mail.ru>
2025-06-23 09:06:16 +0300

    Update reducer.cpp

--- reducer.cpp
+++ reducer.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <string>
+#include <tuple>
+
+std::tuple<size_t, size_t, double> reducer(std::tuple<size_t, size_t, double> prev_data, std::string line)
+{
+    size_t count, sum;
+    std::tie(count, sum, std::ignore) = prev_data;
+    int number = std::stoi(line);
+    count++;
+    sum += number;
+    return std::make_tuple(count, sum, sum / count);
+}
+
 
 int main(int argc, char ** argv)
 {
-    size_t count = 0;
-    size_t sum = 0;
+    std::tuple<size_t, size_t, double> previus_state;
     std::string line;
     while (std::getline(std::cin, line))
     {
-	int number_ = std::stoi(line);
-	sum += number_;
-	count++;
+        previus_state = reducer(previus_state, line);
     }
-  
-    double sredn_ = sum / count;
-    std::cout << sredn_ << std::endl;
+
+    double res;
+    std::tie(std::ignore, std::ignore, res) = previus_state;
+    std::cout << res << std::endl;
     return 0;
 }
